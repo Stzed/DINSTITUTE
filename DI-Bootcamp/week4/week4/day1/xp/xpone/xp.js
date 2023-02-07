37ae999e3cfce07eaca448872aba4d4dd5cd3746
Stzed <113072693+Stzed@users.noreply.github.com>
2023-02-07 21:45:21 +0200

    Add files via upload

---
+++ DI-Bootcamp/week4/week4/day1/xp/xpone/xp.js
@@ -0,0 +1,92 @@
+🌟 Exercise 1 : Scope
+Instructions
+Analyse the code below, and predict what will be the value of a in all the following functions.
+Write your prediction as comments in a js file. Explain your predictions.
+// #1
+function funcOne() {
+    let a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(`inside the funcOne function ${a}`);
+}
+
+// #1.1 - run in the console:
+funcOne()
+// #1.2 What will happen if the variable is declared 
+// with const instead of let ?
+
+
+//const doesnt let redefining so there will be an error
+
+
+
+
+
+//#2
+let a = 0;
+function funcTwo() {
+    a = 5;
+}
+
+function funcThree() {
+    alert(`inside the funcThree function ${a}`);
+}
+
+// #2.1 - run in the console:
+funcThree() //will take the function from 26 = 0
+funcTwo() // undefined because a was defined twice  
+funcThree() // function 2 redifined a so this will be 5
+// #2.2 What will happen if the variable is declared 
+// with const instead of let ?
+
+// error. a cannot be declared in a const and then declared again 
+
+
+
+
+
+
+//#3
+function funcFour() {
+    window.a = "hello";
+}
+
+
+function funcFive() {
+    alert(`inside the funcFive function ${a}`);
+}
+
+// #3.1 - run in the console:
+funcFour()
+funcFive()
+
+//#4
+let a = 1;
+function funcSix() {
+    let a = "test";
+    alert(`inside the funcSix function ${a}`);
+}
+
+
+// #4.1 - run in the console:
+funcSix()
+// #4.2 What will happen if the variable is declared 
+// with const instead of let ?
+
+//#5
+let a = 2;
+if (true) {
+    let a = 5;
+    alert(`in the if block ${a}`);
+}
+alert(`outside of the if block ${a}`);
+
+// #5.1 - run the code in the console
+// #5.2 What will happen if the variable is declared 
+// with const instead of let ?
+
+
+
+
+
---
+++ DI-Bootcamp/week4/week4/day3/xp/xpone/xp.js
@@ -0,0 +1,28 @@
+// Exercise 1 : Analyzing
+// Instructions
+// Analyze these pieces of code before executing them. What will be the outputs ?
+// ------1------
+// const fruits = ["apple", "orange"];
+// const vegetables = ["carrot", "potato"]; 
+
+// const result = ['bread', ...vegetables, 'chicken', ...fruits];
+// console.log(result);
+
+// ------2------
+// const country = "USA";
+// console.log([...country]);
+
+// ------Bonus------
+// let newArray = [...[,,]];
+// console.log(newArray);
+
+
+----1-----
+one Array
+
+----2------
+"USA" array with one value 
+but it avtually divided each letter into an array
+
+----bonus-----
+2 arrays 
