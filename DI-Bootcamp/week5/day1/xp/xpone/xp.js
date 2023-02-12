73024839c1d01823132682192f2ca2d918262b3d
Stzed <113072693+Stzed@users.noreply.github.com>
2023-02-13 00:15:50 +0200

    Add files via upload

---
+++ DI-Bootcamp/week5/day1/xp/xpone/xp.js
@@ -0,0 +1,14 @@
+// Hint : Read about sending form data using the HTTP protocol
+
+// form
+
+// Create a form like the one above. The form should contain three inputs:
+// a small text input asking for a name,
+// a larger textarea input to write a message,
+// a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+// Where will the sent data appear?
+
+ 
+it will appear in the url
---
+++ DI-Bootcamp/week5/day1/xp/xptwo/xp.js
@@ -0,0 +1,7 @@
+// 🌟 Exercise 2 : HTML Form #2
+// Instructions
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+
+it will not appear in the URL, only in the network tab 
